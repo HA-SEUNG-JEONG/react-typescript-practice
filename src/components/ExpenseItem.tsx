eece88173b35740e63fa67a5dab0d1f5e557c3ef
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-05-21 16:59:38 +0900

    props

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props: any) => {
   return (
     <div className="expense-item">
-      <div>March 21th 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
